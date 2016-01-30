c8b67eb3fa3a96dcc248334a8ec667130e5629d1
Dominic Voto <applesdom@gmail.com>
2016-01-29 20:58:21 -0600

    Site overhaul.

--- app.js
+++ app.js
@@ -11,32 +11,39 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, '/public')));
 
 // GET method route
-app.get('/', function (req, res) {
+app.get('/', function(req, res)
+{
   res.render('index.html');
 });
 
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res)
+{
   res.render('test.html');
 });
 
-app.get('/dots', function (req, res) {
+app.get('/dots', function(req, res)
+{
   res.render('dots.html');
 });
 
-app.get('/calc', function (req, res) {
+app.get('/calc', function(req, res)
+{
   res.render('calc.html');
 });
 
-app.get('/missile', function (req, res) {
+app.get('/missile', function(req, res)
+{
   res.render('missile.html');
 });
 
-app.get('/gravity', function (req, res) {
+app.get('/gravity', function(req, res)
+{
   res.render('gravity.html');
 });
 
 var port = process.env.PORT || 3000;
 
-http.createServer(app).listen(port, function(){
+http.createServer(app).listen(port, function()
+{
 	console.log('Server listening on port ' + port);
 });
