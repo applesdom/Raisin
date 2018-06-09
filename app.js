5154b70c8b6f2a6fbb6dc746a885026587f39723
applesdom <applesdom@gmail.com>
2018-06-08 19:58:20 -0500

    Enhance interactivity
    Add background dots to countdown
    Add red text to countdown
    Add favicon
    Change quote

--- app.js
+++ app.js
@@ -1,24 +1,78 @@
-
 var express = require('express');
 var app = express();
-
-var http = require('http');
 var path = require('path');
+var expressWs = require('express-ws')(app);
 
-app.set('views', __dirname + '/');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//Static resources
 app.use(express.static(path.join(__dirname, '/public')));
 
 // GET method route
-app.get('/', function(req, res)
-{
-  res.render('index.html');
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+function Client(ws, x, y) {
+	this.ws = ws;
+	this.x = x;
+	this.y = y;
+}
+
+var clientList = [];
+
+function sendToAll() {
+	for(let client of clientList) {
+		let sendString = "";
+		for(let client2 of clientList) {
+			if(client !== client2) {
+				sendString += client2.x + " " + client2.y + " ";
+			}
+		}
+		sendString += client.x + " " + client.y;
+		client.ws.send(sendString);
+	}
+}
+
+//WebSocket route
+app.ws('/', function(ws, req) {
+	//console.log("New connection from " + ws._socket.remoteAddress);
+	
+	var client = new Client(ws, 0, 0);
+	while(true) {
+		if(clientList.length < 2048) {
+			let dis = 512*Math.pow(Math.random() - 0.5, 5) + 16;
+			//let dis = 128*Math.pow(Math.random() - 0.5, 3) + 16;
+			let dir = Math.random()*2*Math.PI;
+			client.x = Math.floor(Math.cos(dir)*dis + 32);
+			client.y = Math.floor(Math.sin(dir)*dis + 32);
+		} else {
+			client.x = Math.floor(Math.random()*64);
+			client.y = Math.floor(Math.random()*64);
+		}
+		
+		var duplicate = false;
+		for(let client2 of clientList) {
+			if(client2.x === client.x && client2.y === client.y) {
+				duplicate = true;
+				break;
+			}
+		}
+		if(!duplicate) {
+			break;
+		}
+	}
+	clientList.push(client);
+	
+	ws.on('close', function(msg) {
+		//console.log("Client from " + ws._socket.remoteAddress + " disconnected");
+		clientList.splice(clientList.indexOf(client), 1);
+		
+		sendToAll();
+	});
+	
+	sendToAll();
 });
 
 var port = process.env.PORT || 3000;
-
-http.createServer(app).listen(port, function()
-{
+app.listen(port, function() {
 	console.log('Server listening on port ' + port);
 });
---
+++ public/index.js
@@ -0,0 +1,68 @@
+var clock = document.getElementById("clock");
+var canvas = document.getElementById("canvas");
+var g = canvas.getContext("2d");
+
+var duration = 10000000000;
+
+function updateClock() {
+	let time = duration - (new Date().getTime() % duration);
+	
+	let days = Math.floor(time/(24*60*60*1000));
+	let hours = Math.floor(time/(60*60*1000)) % 24;
+	let minutes = Math.floor(time/(60*1000)) % 60;
+	let seconds = Math.floor(time/1000) % 60;
+	let millis = time % 1000;
+	
+    clock.innerHTML = (days + ":" + pad(hours, 2) + ":" + pad(minutes, 2) + ":" + pad(seconds, 2)).replace("44", "<font color=\"red\">44</font>") + "." + pad(millis, 3);
+}
+
+function pad(number, length) {
+    var my_string = '' + number;
+    while (my_string.length < length) {
+        my_string = '0' + my_string;
+    }
+    return my_string;
+}
+
+function Dot(x, y, highlight) {
+	this.x = x;
+	this.y = y;
+	this.highlight = highlight;
+}
+
+var dotList = [];
+
+function updateDots() {
+	g.fillStyle = "#000000";
+	g.fillRect(0, 0, canvas.width, canvas.height);
+	
+	for(let dot of dotList) {
+		if(dot.highlight) {
+			g.fillStyle = "#404040";
+			g.strokeStyle = "#404040";
+		} else {
+			g.fillStyle = "#202020";
+			g.strokeStyle = "#202020";
+		}
+		
+		g.beginPath();
+		g.arc((dot.x*16)+6, (dot.y*16)+6, 6, 0, 2*Math.PI);
+		g.fill();
+		g.stroke();
+	}
+}
+
+var webSocket = new WebSocket("ws://localhost:5000");
+webSocket.onmessage = function (event) {
+	console.log(event.data);
+	let newDotList = [];
+	let split = event.data.split(" ");
+	for(let i = 0; i < split.length - 2; i += 2) {
+		newDotList.push(new Dot(Number.parseFloat(split[i]), Number.parseFloat(split[i+1]), false));
+	}
+	newDotList.push(new Dot(Number.parseFloat(split[split.length - 2]), Number.parseFloat(split[split.length - 1]), true));
+	dotList = newDotList;
+	updateDots();
+}
+
+setInterval(updateClock, 41);
